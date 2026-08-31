c77a21d8e9b3fc27a1feba50925f2e7ac7748f0e
SRINU MUTHU KUMARAN B I <Itzmesrinu09@gmail.com>
2026-08-31 16:13:09 +0530

    Time: 64 ms (44.81%), Space: 107.6 MB (41.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[temperatures.length];
-
         for(int i=0;i<temperatures.length;i++){
             int cday = temperatures[i];
             while(!st.isEmpty() && temperatures[i] > temperatures[st.peek()] ){
