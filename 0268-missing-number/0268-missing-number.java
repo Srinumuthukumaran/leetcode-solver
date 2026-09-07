405d7df74f43afc158dd4a20866167647322f6e1
SRINU MUTHU KUMARAN B I <Itzmesrinu09@gmail.com>
2026-09-07 09:25:18 +0530

    Time: 0 ms (100%), Space: 46.9 MB (97.45%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int actualSum = 0;
+        int expectedSum = n*(n+1) / 2;
+
+        for(int num : nums){
+            actualSum+=num;
+        }
+
+        int missing  = expectedSum - actualSum;
+        return missing;
+    }
+}
