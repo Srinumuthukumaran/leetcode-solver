8e5c927e688d5cbaeb93e7548cbeac1174597911
SRINU MUTHU KUMARAN B I <Itzmesrinu09@gmail.com>
2026-09-09 18:07:44 +0530

    Time: 3 ms (10.05%), Space: 43.6 MB (5.22%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = "";
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch>='A' && ch<='Z'){
+                ans += (char)(ch + 32);
+            }else{
+                ans+=ch;
+            }
+        }
+        return ans;
+    }
+}
