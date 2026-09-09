201fb640be4a34098e57ce1c5c83501b8358123a
SRINU MUTHU KUMARAN B I <Itzmesrinu09@gmail.com>
2026-09-09 19:17:14 +0530

    Time: 1 ms (99.43%), Space: 46.9 MB (41.66%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int[] res = new int[arr.length];
+        int index =0;
+
+        for(int i=0;i<arr.length && index<arr.length;i++){
+            if(arr[i] == 0){
+                index+=2;
+            }
+            else{
+                res[index] = arr[i];
+                index++;
+            }
+        }
+        for(int k=0;k<res.length;k++){
+            arr[k] = res[k];
+        }
+    }
+}
